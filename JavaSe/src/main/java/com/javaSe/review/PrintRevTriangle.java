d496f4b425244774ca0486999ed2bfacf76971ac
ddone <ddone@163.com>
2020-07-15 08:16:38 +0800

    添加倒三角

---
+++ JavaSe/src/main/java/com/javaSe/review/PrintRevTriangle.java
@@ -0,0 +1,25 @@
+package com.javaSe.review;
+
+/**
+ * @author ddone
+ * @date 2020/7/15-00:22
+ */
+public class PrintRevTriangle {
+    public static void main(String[] args) {
+            printRevTriangle(6);
+    }
+
+    public static void printRevTriangle(int num) {
+        for (int i = 0; i < num; i++){
+            for (int j = 0 ;j<=i;j++){
+                System.out.print(" ");
+            }
+            for (int k = 0 ;k<num-i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
