b13f982031932138633e564c3aeda9b6f6200cb2
ddone <ddone@163.com>
2020-07-15 08:33:06 +0800

    杨辉三角

---
+++ JavaSe/src/main/java/com/javaSe/review/PrintYangHuiTriangle.java
@@ -0,0 +1,19 @@
+package com.javaSe.review;
+
+/**
+ * @author ddone
+ * @date 2020/7/15-08:18
+ */
+
+public class PrintYangHuiTriangle {
+    public static void main(String[] args) {
+        
+    }
+    public static void printYangHuiTriangle(int num){
+        int[][] array = new int[num][];
+        for (int i = 0; i < array.length; i++) {
+            array[1] = new int[i + 1];
+
+        }
+    }
+}
